830ecac5f4fe0aa7b32c164d03b9362cb60f5cea
郑泽鑫 <576160471@qq.com>
2016-03-22 21:48:16 +0800

    修改判断结束错误，添加注释

--- ExonExtraction/ExonExtraction.py
+++ ExonExtraction/ExonExtraction.py
@@ -1,20 +1,33 @@
-inputfile=input('要处理的序列的文件名：')
-outputfile=input('输出的文件名：')
+inputFile=input('要处理的序列的文件名：')
+outputFile=input('输出的文件名：')
 line=int(input('序列从第几行开始：'))-1
+# 输入文件的名称，序列开始的行号等
 sequence=''
 p=0
 exon=1
-with open(inputfile,'r')as f1,open(outputfile,'a')as f2:
-    readlines=(f1.readlines()[line:])
-    startline=readlines[0]
-    startnum=int(startline[56:63])+(50-1)
-    for lines in readlines:
+# 初始化，sequence为处理后的序列,p当前碱基在序列中的位置，exon为外显子的序号
+with open(inputFile,'r')as f1,open(outputFile,'a')as f2,open('sequence.txt','w')as f3:
+    readLines=(f1.readlines()[line:])
+    #从定义的第line行开始读取到文件尾
+    startLine=readLines[0]
+    startNum=int(startLine[56:63])+(50-1)
+    #从第一行算出起始的碱基位置
+    #startNum=int(startLine[56:63])-(50-1)
+    #正向链的算法
+    for lines in readLines:
         sequence+=lines[0:54].replace(' ','')
+        #删除序列中的空格，并将序列连起来
+    f3.write(sequence)
+    #将处理后的序列输出到sequence.txt文件
     while p<len(sequence):
         if sequence[p].islower() and sequence[p+1].isupper():
-            startPosition=startnum-p-1
+            startPosition=startNum-p-1
+            #startPosition=startNum+p-1
+            #正向链的算法
         if sequence[p].isupper() and sequence[p+1].islower():
-            endPosition=startnum-p-1
+            endPosition=startNum-p
+            #endPosition=startNum+p
+            #正向链的算法
             f2.write('exon'+str(exon)+'  '+str(startPosition)+'  '+str(endPosition)+'\n')
             exon+=1
         p+=1
