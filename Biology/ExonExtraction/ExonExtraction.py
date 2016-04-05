330cec0104b971fb2eeb24985cdffca934b29720
郑泽鑫 <576160471@qq.com>
2016-04-05 07:05:37 +0000

    解决序列一开始就是外显子的bug

--- Biology/ExonExtraction/ExonExtraction.py
+++ Biology/ExonExtraction/ExonExtraction.py
@@ -1,22 +1,30 @@
+import re
 sequence=''
 exonSequence=''
 p=0
 s=0
 exon=1
-with open('tp53UnigeneExon.txt','r')as f1,open('Exon.txt','w')as f2:
-    readLines=(f1.readlines()[3:])
-    startLine=readLines[0]
-    startNum=int(startLine[56:63])+(50-1)
+with open('tp53UnigeneExon.txt','r')as f1,open('Exon.txt','w')as f2,open('sequence.txt','w')as f3:
+    next(f1)
+    readLines=(f1.readlines())
+    startLine=readLines[2]
+    print(startLine)
+    startNum=int(re.findall('\d+',startLine)[0])+len(re.findall('[A-Za-z]',startLine))-1
+    print(startNum)
     for lines in readLines:
-        sequence+=lines[0:54].replace(' ','')
-    while p<len(sequence):
+        sequence+=''.join((re.findall('[A-Za-z]',lines)))
+    f3.write(sequence)
+    sequence='a'+sequence#在序列前加一个内含子，解决序列一开始就是外显子的bug
+    while p<(len(sequence)-1):
         if (sequence[p].islower() and sequence[p+1].isupper()):
-            startPosition=startNum-(p+1)
+            print(p)
+            startPosition=startNum-(p+1)+1
             s=p+1
             while sequence[s].isupper():
                 exonSequence+=sequence[s]
                 s+=1
-            endPosition=startNum-(s-1)
+            endPosition=startNum-(s-1)+1
             f2.write('exon'+str(exon)+'\t'+str(startPosition)+'\t'+str(endPosition)+'\t'+str(exonSequence)+'\n')
             exon+=1
+            p=s-1
         p+=1
