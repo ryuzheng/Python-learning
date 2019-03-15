1fea9a94f1215ec41530bde2022734b3fd7b72dc
ryuzheng <576160471@qq.com>
2019-03-15 14:37:54 +0800

    leetcode

--- Leetcode/20.有效的括号.py
+++ Leetcode/20.有效的括号.py
@@ -12,47 +12,57 @@
 # Testcase Example:  '"()"'
 #
 # 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
-# 
+#
 # 有效字符串需满足：
-# 
-# 
+#
+#
 # 左括号必须用相同类型的右括号闭合。
 # 左括号必须以正确的顺序闭合。
-# 
-# 
+#
+#
 # 注意空字符串可被认为是有效字符串。
-# 
+#
 # 示例 1:
-# 
+#
 # 输入: "()"
 # 输出: true
-# 
-# 
+#
+#
 # 示例 2:
-# 
+#
 # 输入: "()[]{}"
 # 输出: true
-# 
-# 
+#
+#
 # 示例 3:
-# 
+#
 # 输入: "(]"
 # 输出: false
-# 
-# 
+#
+#
 # 示例 4:
-# 
+#
 # 输入: "([)]"
 # 输出: false
-# 
-# 
+#
+#
 # 示例 5:
-# 
+#
 # 输入: "{[]}"
 # 输出: true
-# 
+#
 #
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-
+        # NOTE: python 栈
+        lefts = {"[", "{", "("}
+        rights = {"]", '}', ")"}
+        stack = []
+        for x in s:
+            if x in lefts:
+                stack.append(x)
+            elif x in rights:
+                if not stack or not (1 <= ord(x) - ord(stack[-1]) <= 2):
+                    return False
+                stack.pop()
+        return not stack
---
+++ Leetcode/21.合并两个有序链表.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (52.93%)
+# Total Accepted:    49.9K
+# Total Submissions: 93.9K
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+#
+# 示例：
+#
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+#
+#
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(0)
+        first = head
+        while l1 is not None and l2 is not None:
+            if l1.val > l2.val:
+                head.next = l2
+                l2 = l2.next
+            else:
+                head.next = l1
+                l1 = l1.next
+            head = head.next
+        if l1 is None:
+            head.next = l2
+        elif l2 is None:
+            head.next = l1
+        return first.next
