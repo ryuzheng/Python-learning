b011e2402201ec14d3198728151e8a00d1f4ac3e
Zexin Zheng <ryuzhengzexin@gmail.com>
2019-11-07 19:05:48 +0800

    Update IPRB.py

--- rosalind/problems/IPRB/IPRB.py
+++ rosalind/problems/IPRB/IPRB.py
@@ -4,7 +4,7 @@
  * @Author: Zexin Zheng
  * @Date: 2019-11-07 17:27:37
  * @Last Modified by: Zexin Zheng
- * @Last Modified time: 2019-11-07 19:03:49
+ * @Last Modified time: 2019-11-07 19:05:29
  */
 '''
 
@@ -15,9 +15,10 @@ import sys
 with open(sys.argv[1], 'r') as fi:
     line = fi.read().strip()
     k, m, n = line.split(' ')
-k=int(k)
-m=int(m)
-n=int(n)
+
+k = int(k)
+m = int(m)
+n = int(n)
 
 total = sum([k, m, n])
 
