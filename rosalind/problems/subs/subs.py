371822f8cf61d1972e4453337e3f3a2150e35861
Zexin Zheng <ryuzhengzexin@gmail.com>
2019-11-11 17:37:37 +0800

    update solve problem "subs"

--- rosalind/problems/subs/subs.py
+++ rosalind/problems/subs/subs.py
@@ -1,2 +1,16 @@
-with open ('rosalind_subs.txt','r')as f1:
-    
+# -*- coding: utf-8 -*-
+'''
+/*
+ * @Author: Zexin Zheng
+ * @Date: 2019-11-11 17:29:59
+ * @Last Modified by: Zexin Zheng
+ * @Last Modified time: 2019-11-11 17:36:02
+ */
+'''
+
+with open('rosalind_subs.txt', 'r') as f1:
+    string = f1.readline().strip()
+    substring = f1.readline().strip()
+    # print(string, substring)
+
+print(' '.join([str(i + 1) for i in range(len(string)) if string.startswith(substring, i)]))
